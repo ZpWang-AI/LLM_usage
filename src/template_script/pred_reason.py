1b2ed5f9a747965726ec961755444dc8dab8659f
jiah-li <lijiahuiim@gmail.com>
2024-06-28 12:06:52 +0800

    modify scripts

---
+++ src/template_script/pred_reason.py
@@ -0,0 +1,53 @@
+from utils_zp.common_import import *
+
+add_sys_path(__file__, 2)
+
+SRC_PATH = path(__file__).parent.parent
+ROOT_PATH = SRC_PATH.parent
+
+from IDRR_data import IDRRDataFrames
+from generate_reasoning import ReasoningGenerator
+# from process_pred import ReasoningPredProcessor
+
+
+def main():
+    prompt = [
+        '''
+    Argument 1:
+    {arg1}
+
+    Argument 2:
+    {arg2}
+
+    What's the implicit meaning between the arguments?
+        '''.strip(),
+        '''
+    What is the discourse relation between Argument 1 and Argument 2?
+    A. Comparison
+    B. Contingency
+    C. Expansion
+    D. Temporal
+
+    Answer:
+        '''.strip(),
+    ]
+    
+    dfs = IDRRDataFrames(
+        data_name='pdtb3', data_level='top', data_relation='Implicit',
+        data_path='/home/qwe/test/zpwang/IDRR_data/data/used/pdtb3_top_implicit.subtext2.csv'
+    )
+    sample_generator = ReasoningGenerator(
+        prompt=prompt,
+        llm_name='gpt-4-turbo',
+        desc=f'subtext',
+        output_space='/home/qwe/test/zpwang/LLM_Reasoning/data/reasoning',
+        dfs=dfs,
+        split='test',
+        n_reasoning_per_sample=1,
+        max_sample=-1,
+    )
+    sample_generator.start()
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ src/template_script/run.sh
@@ -0,0 +1,21 @@
+script_dir="/home/qwe/test/zpwang/LLM_Reasoning/src/scripts"
+log_dir="/home/qwe/test/zpwang/LLM_Reasoning/logs"
+
+torun_file=$(find $script_dir -type f -name "${1}*")
+num_files=$(echo $torun_file | wc -l)
+if [ $num_files -ne 1 ]; then
+    echo $torun_file
+    echo "wrong num of files"
+fi
+
+start_time=$(date +%Y-%m-%d-%H-%M-%S)
+filename=$(basename "$torun_file")
+filename="${filename%.*}"
+echo $torun_file
+echo "start running"
+
+# source activate zpwang_llama
+export MKL_SERVICE_FORCE_INTEL=1
+nohup /home/qwe/miniconda3/envs/zpwang_llama/bin/python $torun_file > "${log_dir}/${start_time}.${filename}.log" 2>&1 &
+ps -aux | grep python | grep zpwang | grep -v gpustat | grep -v .vscode-server
+
